e08c7b63f8e4ccd0b71625f0c251d74d962dd3dd
mmillheim <m.millheim@gmail.com>
2021-09-08 19:23:08 -0400

    migrations and admin

--- capture_pad/admin.py
+++ capture_pad/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Notebook, Year, Month, Day, Note
+
+admin.site.register(User)
+admin.site.register(Notebook)
+admin.site.register(Year) 
+admin.site.register(Month) 
+admin.site.register(Day) 
+admin.site.register(Note)
 
 # Register your models here.
--- capture_pad_django/settings.py
+++ capture_pad_django/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'capture_pad',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
